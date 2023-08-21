2cbc2c23ae7b8dbaeb8ec1fea401b1dbfdb992c6
tdzard94 <td.zard@gmail.com>
2023-08-21 15:27:04 +0700

    cc

--- updaterepo.sh
+++ updaterepo.sh
@@ -77,13 +77,13 @@ if [[ "$OSTYPE" == "linux"* ]]; then # Linux usage of repo.me
 
     rm {Packages{,.xz,.gz,.bz2,.zst},Release{,.gpg}} 2> /dev/null
 
-    ./apt-ftparchive packages ./debians > Packages
+    apt-ftparchive packages ./debians > Packages
     gzip -c9 Packages > Packages.gz
     xz -c9 Packages > Packages.xz
     zstd -c19 Packages > Packages.zst
     bzip2 -c9 Packages > Packages.bz2
 
-    ./apt-ftparchive release -c ./assets/repo/repo.conf . > Release
+    apt-ftparchive release -c ./assets/repo/repo.conf . > Release
 
     echo "Repository Updated, thanks for using repo.me!"
 else
